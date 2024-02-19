cfe7f0d4d461d75265a1bca3e1219206acdcc33f
Martin <93053179+mjospovich@users.noreply.github.com>
2024-02-19 17:50:09 +0100

    added start and end dates on calendars

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,5 @@
 //* Functions and variables for both calendars
+
 months = [
   "January",
   "February",
@@ -14,6 +15,10 @@ months = [
   "December",
 ];
 
+let selectedDateStart;
+let selectedDateEnd;
+
+// Function that renders calendars
 const renderCalendar = (calendar, year, month, daysTag, num) => {
   let firstDay = new Date(year, month, 1).getDay();
   let lastDate = new Date(year, month + 1, 0).getDate();
@@ -29,28 +34,64 @@ const renderCalendar = (calendar, year, month, daysTag, num) => {
   for (let i = 1; i <= lastDate; i++) {
     let today = i;
 
+    const isSelectedStart =
+      selectedDateStart &&
+      selectedDateStart.getDate() === today &&
+      selectedDateStart.getMonth() === month &&
+      selectedDateStart.getFullYear() === year;
+    const isSelectedEnd =
+      selectedDateEnd &&
+      selectedDateEnd.getDate() === today &&
+      selectedDateEnd.getMonth() === month &&
+      selectedDateEnd.getFullYear() === year;
+
     if (
       today === new Date().getDate() &&
       year === new Date().getFullYear() &&
       month === new Date().getMonth()
     ) {
-      tag += `<li class="today">${i}</li>`;
-    } 
-    else {
-      tag += `<li>${i}</li>`;
+      tag += `<li class="today${isSelectedStart ? " select_start" : ""}${
+        isSelectedEnd ? " select_end" : ""
+      }">${i}</li>`;
+    } else {
+      tag += `<li${isSelectedStart ? ' class="select_start"' : ""}${
+        isSelectedEnd ? ' class="select_end"' : ""
+      }>${i}</li>`;
     }
-  }
-
-  for (let i = lastDay; i < 6; i++) {
-    tag += `<li class="other_month_${num}">${i - lastDay + 1}</li>`;
   }
 
   calendar.innerHTML = `${months[month]} ${year}`;
   daysTag.innerHTML = tag;
+
+  daysTag.querySelectorAll("li").forEach((day) => {
+    day.addEventListener("click", () => {
+      const dayNumber = parseInt(day.textContent, 10);
+
+      if (num == 1) {
+        selectedDateStart = new Date(year, month, dayNumber);
+        //alert(selectedDateStart);
+
+        daysTag.querySelectorAll("li").forEach((day) => {
+          day.classList.remove("select_start");
+        });
+
+        day.classList.add("select_start");
+      } else if (num == 2) {
+        selectedDateEnd = new Date(year, month, dayNumber);
+        //alert(selectedDateEnd);
+
+        daysTag.querySelectorAll("li").forEach((day) => {
+          day.classList.remove("select_end");
+        });
+
+        day.classList.add("select_end");
+      }
+    });
+  });
 };
 
-//* script for first calendar
-const currentDate1 = document.querySelector(".current_date_1");
+//* Script for first calendar
+const currDate1 = document.querySelector(".current_date_1");
 let daysTag1 = document.querySelector(".days_1");
 let prevNextIcon1 = document.querySelectorAll(".icons_1 span");
 
@@ -59,27 +100,26 @@ let currYear1 = date1.getFullYear();
 let currMonth1 = date1.getMonth();
 //console.log(date, currYear, currMonth);
 
-renderCalendar(currentDate1, currYear1, currMonth1, daysTag1, 1);
+renderCalendar(currDate1, currYear1, currMonth1, daysTag1, 1);
 
 prevNextIcon1.forEach((icon) => {
   icon.addEventListener("click", () => {
     currMonth1 = icon.id === "prev1" ? currMonth1 - 1 : currMonth1 + 1;
 
-    if(currMonth1 < 0 || currMonth1 > 11){
+    if (currMonth1 < 0 || currMonth1 > 11) {
       date1 = new Date(currYear1, currMonth1);
       currYear1 = date1.getFullYear();
       currMonth1 = date1.getMonth();
-    }
-    else{
+    } else {
       date1 = new Date();
     }
 
-    renderCalendar(currentDate1, currYear1, currMonth1, daysTag1, 1);
+    renderCalendar(currDate1, currYear1, currMonth1, daysTag1, 1);
   });
 });
 
-//* script for second calendar
-const currentDate2 = document.querySelector(".current_date_2");
+//* Script for second calendar
+const currDate2 = document.querySelector(".current_date_2");
 let daysTag2 = document.querySelector(".days_2");
 let prevNextIcon2 = document.querySelectorAll(".icons_2 span");
 
@@ -88,26 +128,23 @@ let currYear2 = date2.getFullYear();
 let currMonth2 = date2.getMonth();
 //console.log(date, currYear, currMonth);
 
-renderCalendar(currentDate2, currYear2, currMonth2, daysTag2, 2);
+renderCalendar(currDate2, currYear2, currMonth2, daysTag2, 2);
 
 prevNextIcon2.forEach((icon) => {
   icon.addEventListener("click", () => {
     currMonth2 = icon.id === "prev2" ? currMonth2 - 1 : currMonth2 + 1;
 
-    if(currMonth2 < 0 || currMonth2 > 11){
+    if (currMonth2 < 0 || currMonth2 > 11) {
       date2 = new Date(currYear2, currMonth2);
       currYear2 = date2.getFullYear();
       currMonth2 = date2.getMonth();
-    }
-    else{
+    } else {
       date2 = new Date();
     }
 
-    renderCalendar(currentDate2, currYear2, currMonth2, daysTag2, 2);
+    renderCalendar(currDate2, currYear2, currMonth2, daysTag2, 2);
   });
 });
-
-
 
 //* Fetchin API data for employee dropdown
 document.addEventListener("DOMContentLoaded", function () {
