2fdc3c5e3e410e6d22fd3aa148a55181dbf78fce
Martin <93053179+mjospovich@users.noreply.github.com>
2024-02-21 14:57:48 +0100

    added async for fetch

--- Scripts/employees.js
+++ Scripts/employees.js
@@ -1,18 +1,42 @@
 //! Employees script
 
-//* Fetchin API data for employee dropdown
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((data) => {
-      const dropdown = document.getElementById("user_dropdown");
-      data.forEach((user) => {
-        const option = document.createElement("option");
-        option.value = user.id;
-        option.textContent = user.name;
-        dropdown.appendChild(option);
-      });
-    })
+//* Function for fetchin API data for employee dropdown
+async function fetchEmployees() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    
+    const employees = [];
 
-    .catch((error) => console.error(error));
-});
+    // Object for each employee
+    data.forEach(employee => {
+      const employeeObject = {
+        id: employee.id,
+        name: employee.name,
+        email: employee.email,
+        ptos: []
+      };
+
+      // Then store it to array
+      employees.push(employeeObject);
+    });
+
+    // Store the array to local storage
+    localStorage.setItem("employees", JSON.stringify(employees));
+
+    // For options in the dropdown
+    const dropdown = document.getElementById("user_dropdown");
+    employees.forEach((employee) => {
+      const option = document.createElement("option");
+      option.value = employee.id;
+      option.textContent = employee.name;
+      dropdown.appendChild(option);
+    });
+    
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Call fetch for employees
+document.addEventListener("DOMContentLoaded", fetchEmployees);
